929670a005958e689b0e0622789a09028349cf7e
JOSHUA IKULAYO <ikulayojoshua1503@gmail.com>
2024-08-16 23:10:52 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,28 @@ export const App = () => {
       <div className='fixed top-00 -z-10 h-full w-full'>
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
 </div>
+import PreLoader from './components/PreLoader';
+import './App.css'; // Your main styles
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a data fetching or loading process
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // Adjust the timeout to match your actual loading time
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="App">
+      {loading ? <PreLoader /> : <div>Your main content goes here.</div>}
+    </div>
+  );
+};
+
 <div className='container mx-auto px-8'>
   <Navbar />
   <Hero />
