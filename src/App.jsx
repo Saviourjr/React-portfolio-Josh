fe9e31c8c8482aa2ec5f79a1a1ef561467420885
JOSHUA IKULAYO <ikulayojoshua1503@gmail.com>
2024-09-15 22:45:55 +0100

    Revert "first commit"
    This reverts commit 5a6b96702cd8a0b7d51e365dd299e752f3a07a30.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
 
+import React from 'react'
 
 import Hero from './Components/Hero';
 import About from './Components/About'
@@ -17,6 +17,7 @@ export const App = () => {
       <div className='fixed top-00 -z-10 h-full w-full'>
       <div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
 </div>
+
 <div className='container mx-auto px-8'>
  
   <Hero />
