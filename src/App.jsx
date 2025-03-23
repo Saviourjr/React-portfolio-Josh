c1fb966b5e1bbd46b1b4bec40316ff84f6eadcd3
JOSHUA IKULAYO <ikulayojoshua1503@gmail.com>
2025-03-23 21:05:58 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,29 @@
-
-import React from 'react'
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-=======
->>>>>>> parent of 5a6b967 (first commit)
-import Navbar from './Components/Navbar'
-=======
->>>>>>> fe9e31c8c8482aa2ec5f79a1a1ef561467420885
-import Hero from './Components/Hero';
-import About from './Components/About'
-import Technologies from './Components/Technologies';
-import Education from './Components/Education';
-import { EDUCATIONS } from './constants';
-import Projects from './Components/Projects';
-import { CONTACT } from './constants';
-import Contacts from './Components/Contacts';
-
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import About from "./Components/About";
+import Technologies from "./Components/Technologies";
+import Education from "./Components/Education";
+import { EDUCATIONS } from "./constants";
+import Projects from "./Components/Projects";
+import Contacts from "./Components/Contacts";
 
 export const App = () => {
   return (
-    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
-      <div className='fixed top-00 -z-10 h-full w-full'>
-      <div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
-</div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> fe9e31c8c8482aa2ec5f79a1a1ef561467420885
-=======
-import PreLoader from './components/PreLoader';
-import './App.css'; // Your main styles
-
-const App = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate a data fetching or loading process
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000); // Adjust the timeout to match your actual loading time
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  return (
-    <div className="App">
-      {loading ? <PreLoader /> : <div>Your main content goes here.</div>}
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="fixed top-00 -z-10 h-full w-full">
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      </div>
+      <div className="container mx-auto px-8">
+        <Navbar />
+        <Hero />
+        <About />
+        <Technologies />
+        <Education />
+        <Projects />
+        <Contacts />
+      </div>
     </div>
   );
 };
-
->>>>>>> parent of 5a6b967 (first commit)
-<div className='container mx-auto px-8'>
- 
-  <Hero />
-  <About />
-<Projects />
-<Contacts />
-
-  </div>
-    </div>
-  );
-}
-export default App
+export default App;
--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -1,35 +1,41 @@
-import React from 'react'
-import { CONTACT } from '../constants'
-import { motion } from 'framer-motion'
-import Image from '../assets/birthday.png'
+import React from "react";
+import { CONTACT } from "../constants";
+import { motion } from "framer-motion";
 const Contacts = () => {
   return (
-    <div className='border-b border-neutral-900'>
-     <motion.h1
-     whileInView={{opacity:1, y:0}}
-     initial={{opacity:0, y:-100}}
-     transition={{duration:0.5}}
-     className='my-10 text-center text-4xl'>Birthday Wish</motion.h1>
-     <div
-     
-     className='text-center tracking-tighter'>
+    <div className="border-b border-neutral-900 ">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-10 text-center text-4xl  hover:scale-105 transition-transform duration-150 shadow-sm"
+      >
+        Get In Touch
+      </motion.h1>
+      <div className="text-center tracking-tighter  hover:scale-105 transition-transform duration-300 shadow-sm">
         <motion.p
-        whileInView={{opacity:1, x:0}}
-     initial={{opacity:0, x: 100}}
-     transition={{duration:1}}
-        className='my-4'>{CONTACT.address}</motion.p>
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          {CONTACT.address}
+        </motion.p>
 
-<motion.p
-        whileInView={{opacity:1, x:0}}
-     initial={{opacity:0, x: 100}}
-     transition={{duration:1}}
-        
-        className='my-4'>{CONTACT.phoneNo}</motion.p>
-         <a href="#" className='border-b'>{CONTACT.email}</a>
-         
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          {CONTACT.phoneNo}
+        </motion.p>
+        <a href="#" className="border-b">
+          {CONTACT.email}
+        </a>
+      </div>
     </div>
-   <img src={Image} alt="" /> </div>
-  )
-}
+  );
+};
 
-export default Contacts
+export default Contacts;
